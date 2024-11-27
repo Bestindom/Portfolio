4624951285f3a549c01d86ed1ee5d1a841bf9e08
Eviam Albuja <bestindom@gmail.com>
2024-11-27 14:32:25 +0100

    Frontend Experiencie section

---
+++ src/components/sections/Experiencie.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <ol class="group/list">
+            <li class="mb-12">
+                <div
+                    class="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                    <!-- Click action to redirect -->
+                    <div
+                        class="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg">
+                    </div>
+                    <header
+                        class="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">
+                        2021 - present</header>
+                    <div class="z-10 sm:col-span-6">
+                        <h1>Developer · Pastacuita</h1>
+                        <p>
+                            Meticulous and detail-oriented person, always focused on finding efficient
+                            and scalable solutions. I consider myself curious, resilient, and
+                            cheerful.
+                            <br />
+                            <br />
+                            Moreover, my experience as a competitive underwater hockey player has
+                            taught me the importance of discipline and teamwork, skills that I apply
+                            in my projects.
+                        </p>
+                    </div>
+                </div>
+            </li>
+        </ol>
+    </div>
+</template>
+<script setup>
+    
+</script>
+<style></style>
